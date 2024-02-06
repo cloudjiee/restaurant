4e8baec28d1d82ec518974a796dbbf3e73bc2a2f
DESKTOP-AI6JUPI\88692 <cloud199448@gmail.com>
2024-02-06 13:12:19 +0800

    git init

---
+++ shop/src/test/java/com/ispan/restaurant/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.restaurant.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
